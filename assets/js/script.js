037928514c3db16dbcbf3b3f947247d67a0ed584
Annie Kwan <akwan.mtl@gmail.com>
2020-10-19 20:23:44 -0400

    the quiz portion is now functioning with dummy questions/choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
-var startQuizBtn = document.getElementById("startQuiz");
-var timeLeft = 75;
-var timerEl = document.getElementById("timer");
+// declaring all elements 
 
+var timerEl = document.getElementById("timer");
 var topBar = document.getElementById("topBar");
+
+var startQuizBtn = document.getElementById("startQuiz");
 
 var welcomePage = document.getElementById("welcomePage");
 var quizPage = document.getElementById("quizPage");
 var scorePage = document.getElementById("scorePage");
 var highscorePage = document.getElementById("highscorePage");
 
-
 var feedbackSection = document.getElementById("feedback");
 
+var questionText = document.getElementById("question");
 
-var btn1 = document.getElementById("choice1");
-var btn2 = document.getElementById("choice2");
-var btn3 = document.getElementById("choice3");
-var btn4 = document.getElementById("choice4");
+var btnArray = document.getElementsByClassName("choices");
+
+var scoreText = document.getElementById("score");
 
 var submitBtn = document.getElementById("submit");
 
@@ -24,24 +24,53 @@ var goBackBtn = document.getElementById("goBack");
 var clearBtn = document.getElementById("clear");
 
 var timerID;
+var timeLeft = 75;
 
 
+// list of question objects
+var questions = [
+    {
+        question: "Question 1",
+        choices: ["answer 1", "answer 2","answer 3", "answer 4"],
+        answer: "answer 2"
+    },
+    {
+        question: "Question 2",
+        choices:["answer 5", "answer 6","answer 7", "answer 8"],
+        answer: "answer 8"
+    },
+    {
+        question: "Question 3",
+        choices:["answer a", "answer b","answer c", "answer d"],
+        answer: "answer c"
+    }
+
+];
+var questionsTotal = questions.length; 
+var questionNumber = 0;
+
+// function that starts the quiz and calls on timer
 function startQuiz() {
 
     timerEl.innerText = "Time: " + timeLeft;
     startTimer();
 
+    questionText.innerText = questions[questionNumber].question;
+    for (var i = 0; i < 4; i++){
+        btnArray[i].innerText = questions[questionNumber].choices[i];
+    }
+    
     welcomePage.style.display="none";
     quizPage.style.display="block";
 
-    
 }
 
+// function that starts timer countdown
 function startTimer(){
     var frame = function (){
         
         if(timeLeft <= 0){
-            clearInterval(timerID); 
+            endQuiz();
         }
         else{
             timeLeft--;
@@ -51,18 +80,33 @@ function startTimer(){
     timerID = setInterval(frame,1000);
 }
 
-function answerQuestion(num){
-    
-    if(num===4){
-        timeLeft-=10;
+// function for when user chooses and answer
+function answerQuestion(answer){
+    if(answer === questions[questionNumber].answer){
+        correct();
+    }
+    else {
+        timeLeft -= 10;
+        timerEl.innerText = "Time: " + timeLeft;
         wrong();
     }
+    questionNumber++;
+    if (questionNumber === questionsTotal){
+        endQuiz();
+    }
+    else{
+        questionText.innerText = questions[questionNumber].question;
+        for (var i = 0; i < 4; i++){
+            btnArray[i].innerText = questions[questionNumber].choices[i];
+        }
+    }    
 }
 
 function endQuiz(){
     
     clearInterval(timerID); 
 
+    scoreText.innerText = timeLeft;
     quizPage.style.display="none";
     scorePage.style.display="block";
 
@@ -107,30 +151,13 @@ function wrong(){
 // Add event listener to startQuiz button
 startQuizBtn.addEventListener("click", startQuiz);
 
-
-// btn1.addEventListener("click", function(){
-//     answerQuestion(1);
-//     });
-
+for(var i = 0; i < btnArray.length; i++){
     
-btn1.addEventListener("click", endQuiz);
-
-        
-// btn2.addEventListener("click", function(){
-//     answerQuestion(2);
-//     });
-
-
-btn2.addEventListener("click", correct);
-
-btn3.addEventListener("click", function(){
-    answerQuestion(3);
+    btnArray[i].addEventListener("click",function(){
+        answerQuestion(this.innerText);
     });
-btn4.addEventListener("click", function(){
-    answerQuestion(4)
-    });
+}
 
-    
 submitBtn.addEventListener("click", showScore);
 
 
